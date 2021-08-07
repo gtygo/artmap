5d22c617340b3eec56ac08576bc79ec387604543
gtygo <gtydoit@gmail.com>
2021-08-07 19:17:21 +0800

    impl lock

--- internal/lock.go
+++ internal/lock.go
@@ -1,1 +1,71 @@
 package internal
+
+import (
+	"runtime"
+	"sync/atomic"
+)
+
+const d_flag = uint64(1)
+const l_flag = uint64(1) << 1
+const l_and_d_flag = uint64(1)<<2 - 1
+const spin_count=int8(64)
+
+func rLock(n *n, version *uint64) bool {
+	v:=waitUnLock(n)
+	if (v&d_flag)==d_flag{
+		*version=0
+		return false
+	}
+	*version=v
+	return false
+}
+
+func rUnLock(n *n,version *uint64)bool{
+	if n==nil{
+		return true
+	}
+	return *version==atomic.LoadUint64(&n.version)
+}
+
+func lock(n *n)bool {
+	v := uint64(0)
+	for {
+		if !rLock(n, &v) {
+			return false
+		}
+		if upgrade(n,&v){
+			break;
+		}
+	}
+	return false
+}
+
+func unlock(n *n) {
+	if n == nil {
+		return
+	}
+	atomic.AddUint64(&n.version, l_flag)
+}
+
+func upgrade(n *n ,version *uint64)bool{
+	if n ==nil{
+		return true
+	}
+	return atomic.CompareAndSwapUint64(&n.version,*version,(*version)+l_flag)
+}
+
+func waitUnLock(n *n)uint64{
+	v:=atomic.LoadUint64(&n.version)
+	c:=spin_count
+	for(v & l_flag) == l_flag {
+		if c<=0{
+			runtime.Gosched()
+			c=spin_count
+		}
+		c--
+		v=atomic.LoadUint64(&n.version)
+	}
+	return v
+}
+
+
--- internal/node.go
+++ internal/node.go
@@ -1,4 +1,37 @@
 package internal
 
+import "unsafe"
 
+const maxPrefixLen = 8
 
+type n struct {
+	typ        uint8
+	numChild   uint8
+	prefixLen  uint32
+	version    uint64
+	prefixLeaf uint64
+	prefix     [maxPrefixLen]byte
+}
+
+type n4 struct {
+	n     n
+	keys  [4]byte
+	child [4]unsafe.Pointer
+}
+
+type n16 struct {
+	n     n
+	keys  [16]byte
+	child [16]unsafe.Pointer
+}
+
+type n48 struct {
+	n     n
+	keys  [256]byte
+	child [48]unsafe.Pointer
+}
+
+type n256 struct {
+	n     n
+	child [256]unsafe.Pointer
+}
